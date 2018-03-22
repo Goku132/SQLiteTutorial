fc0eaf9565a3ce68120a86fd4f72beb4be52e30c
Michael Griffin <michealgrffn085@gmail.com>
2018-03-22 14:16:47 +0000

    Added Buttons and alert action working on Auto Layout

--- SQTutorial/ViewController.swift
+++ SQTutorial/ViewController.swift
@@ -20,6 +20,64 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func createTable(_ sender: UIButton) {
+        print("Created Tapped")
+    }
+    
+    @IBAction func insertUser(_ sender: UIButton) {
+        print("Insert Tapped")
+        let alert = UIAlertController(title: "Insert Users", message: nil, preferredStyle: .alert)
+        alert.addTextField { (tf) in
+            tf.placeholder = "Name"
+        }
+        alert.addTextField { (tf) in
+            tf.placeholder = "Email"
+        }
+        let action = UIAlertAction(title: "Submit", style: .default) { (_) in
+            guard let name = alert.textFields?.first?.text, let email = alert.textFields?.last?.text else{ return}
+            print(name)
+            print(email)
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func listUser(_ sender: UIButton) {
+        print("List Tapped")
+    }
+    
+    @IBAction func updateUsers(_ sender: UIButton) {
+        print("Update Tapped")
+        let alert = UIAlertController(title: "Update Users", message: nil, preferredStyle: .alert)
+        alert.addTextField { (tf) in
+            tf.placeholder = "User ID"
+        }
+        alert.addTextField { (tf) in
+            tf.placeholder = "Email"
+        }
+        let action = UIAlertAction(title: "Submit", style: .default) { (_) in
+            guard let userIdString = alert.textFields?.first?.text, let email = alert.textFields?.last?.text else{ return}
+            print(userIdString)
+            print(email)
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func deleteUsers(_ sender: UIButton) {
+        print("Delete Tapped")
+        let alert = UIAlertController(title: "Delete User", message: nil, preferredStyle: .alert)
+        alert.addTextField { (tf) in
+            tf.placeholder = "UserID"
+        }
+        let action = UIAlertAction(title: "Submit", style: .default) { (_) in
+            guard let userIdString = alert.textFields?.first?.text else{ return}
+            print(userIdString)
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
